c7e6ffe59658489285d7a8fee954027ba8fd74a8
YooHayoung <yoo971202@naver.com>
2022-10-01 20:42:11 +0900

    test : 테스트 추가
    cicd 테스트를 위한 실패 테스트 추가

---
+++ backend/src/test/kotlin/com/example/backend/MyTest.kt
@@ -0,0 +1,14 @@
+package com.example.backend
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MyTest {
+
+    @Test
+    fun test1() {
+        val boolean = true
+
+        assertThat(boolean).isFalse
+    }
+}
